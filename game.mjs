310e8742a76b8def91822ebd982db23d5cef2b05
Oskar <oskardb@uia.no>
2024-11-28 22:24:06 +0100

    added initialize function for splashscreen

--- game.mjs
+++ game.mjs
@@ -1,5 +1,7 @@
 import Labyrinth from "./labyrint.mjs"
+import SplashScreen from "./splashScreen.mjs";
 import ANSI from "./utils/ANSI.mjs";
+
 
 const REFRESH_RATE = 250;
 
@@ -10,9 +12,11 @@ let isBlocked = false;
 let state = null;
 
 function init() {
-    //All levels available to the game. 
-    state = new Labyrinth();
-    intervalID = setInterval(update, REFRESH_RATE);
+    const splash = new SplashScreen();
+    splash.animate(() => {
+        state = new Labyrinth();
+        intervalID = setInterval(update, REFRESH_RATE);
+    });
 }
 
 function update() {
