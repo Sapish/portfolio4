649c95c51e126c0a4719e1c10f3ed7ba8fc76259
Oskar <oskardb@uia.no>
2024-11-28 22:19:21 +0100

    splash screen

--- splashScreen.mjs
+++ splashScreen.mjs
@@ -19,9 +19,28 @@ const outputGraphics = `
 class SplashScreen {
 
     constructor() {
-
+        this.isDirty = true;
     }
 
+    animate(onComplete) {
+        const lines = outputGraphics.trim().split("\n");
+        let frameIndex = 0;
+
+        const intervalID = setInterval(() => {
+            console.log(ANSI.CLEAR_SCREEN, ANSI.CURSOR_HOME);
+
+            for (let i = 0; i <= frameIndex; i++) {
+                console.log(ANSI.COLOR.YELLOW + lines[i] + ANSI.COLOR_RESET);
+            }
+
+            frameIndex++;
+            if(frameIndex >= lines.length) {
+                clearInterval(intervalID);
+                console.log(ANSI.COLOR_SCREEN);
+                onComplete();
+            }
+        }, 300);
+    }
     update() {
 
     }
